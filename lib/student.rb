acd069f09458b0585600c89c754649ca967af738
Christie Yoon <cleex1999@gmail.com>
2020-08-27 21:46:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -81,6 +81,18 @@ class Student
     end.first
   end
 
+  def self.all_students_in_grade_X
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+       self.new_from_db(row)
+    end
+  end
+
    def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
